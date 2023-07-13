71ae1ac473783fca011cc5e3ec5fa7ec01b0126a
hridesh kumar <ch.hridesh@gmail.com>
2023-07-13 10:54:17 +0530

    oauth implementation in spring boot

---
+++ src/main/java/com/oauth2/SpringbootOAuth2/SpringbootOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.oauth2.SpringbootOAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOAuth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oauth2/SpringbootOAuth2/SpringbootOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2.SpringbootOAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
